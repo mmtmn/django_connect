d0d64026c23c67a1d004fdc54a38597e1cd22b88
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2020-06-30 01:19:05 -0300

    Add more to component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+        counter: 0
+    };
+
     render() { 
         return (
             <div>
-                <h1>Hello World from a Component!</h1>
-                <button>Increment</button>
+                <h1>This is a Django to React connection</h1>
+                <br></br>
+                <span>{this.formatCount()}</span>
+                <button>Useless button for now</button>
             </div>
+        );
+    }
+
+    formatCount() {
+        return (
+        <h2>is this working?</h2>
         );
     }
 }
